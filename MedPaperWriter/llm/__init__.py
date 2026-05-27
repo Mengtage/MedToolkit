0fa30ab9062376099b9d6343c35e030453752398
Mengtage <lanchuan_2020@qq.com>
2026-05-27 18:08:30 +0800

    Delete MedPaperWriter directory

--- MedPaperWriter/llm/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-LLM模块
-"""
-
-from llm.client import LLMClient, ExpertFactory, TokenUsage, LLMError
-
-__all__ = ["LLMClient", "ExpertFactory", "TokenUsage", "LLMError"]
--- MedPaperWriter/routers/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-API路由模块
-"""
-
-from routers import mode, chat, review, export
-
-__all__ = ["mode", "chat", "review", "export"]
